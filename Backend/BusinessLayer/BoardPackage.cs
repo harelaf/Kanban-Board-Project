6dd4bdf43f378b5beba1cbfed320fa4f8c589b30
Harel Afriat <harelaf@post.bgu.ac.il>
2020-04-13 17:14:44 +0300

    Adding finished column and changing task.cs to BoardPackage.cs

--- Backend/BusinessLayer/BoardPackage.cs
+++ Backend/BusinessLayer/BoardPackage.cs
@@ -8,6 +8,65 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
 {
     namespace BoardPackage
     {
+
+        class Column
+        {
+            List<Task> taskList;
+            int limit;
+
+            public Column()
+            {
+                taskList = new List<Task>();
+                limit = -1;
+            }
+
+            public Task AddTask(string title, string description, DateTime dueDate)
+            {
+                if (taskList.Count == limit)
+                {
+                    throw new Exception("Can't add new task, column has a limit of " + limit);
+                }
+                Task toAdd = new Task(title, description, dueDate);
+                taskList.Add(toAdd);
+                return toAdd;
+            }
+
+            public Task RemoveTask(int taskId)
+            {
+                Task toRemove = taskList.ElementAt(taskId);
+                taskList.Remove(toRemove);
+                return toRemove;
+            }
+
+            public void UpdateTaskDueDate(int taskId, DateTime dueDate)
+            {
+                Task toUpdate = taskList.ElementAt(taskId);
+                toUpdate.UpdateTaskDueDate(dueDate);
+            }
+
+            public void UpdateTaskTitle(int taskId, string title)
+            {
+                Task toUpdate = taskList.ElementAt(taskId);
+                toUpdate.UpdateTaskTitle(title);
+            }
+
+            public void UpdateTaskDescription(int taskId, string description)
+            {
+                Task toUpdate = taskList.ElementAt(taskId);
+                toUpdate.UpdateTaskDescription(description);
+            }
+
+            public void SetLimit(int newLim)
+            {
+                if (taskList.Count > newLim)
+                {
+                    throw new Exception("This column already has more tasks than the new limit");
+                }
+                this.limit = newLim;
+            }
+
+        }
+
         class Task
         {
 
