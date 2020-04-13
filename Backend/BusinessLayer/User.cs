a7e968bc34a21be7f2a3816ae7d03012e90b02e8
mamrud <mamrud@post.bgu.ac.il>
2020-04-13 16:36:32 +0300

    upload user class

---
+++ Backend/BusinessLayer/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Backend.BusinessLayer
+{
+    namespace userPackage
+    {
+        class user
+        {
+            private String email;
+            private string password;
+            private string nickname;
+            private myBoard Board;
+
+
+            public user(string email, string password, string nickname)
+            {
+                this.email = email;
+                this.password = password;
+                this.nickname = nickname;
+                myBoard = new BoardPackage.Board();
+            }
+
+            public Boolean validatePassword(string password)
+            {
+                return this.password == password;
+            }
+
+            public string GetEmail()
+            {
+                return email;
+            }
+
+            public Board GetBoard()
+            {
+                return Board;
+            }
+        }
+    }
+}
