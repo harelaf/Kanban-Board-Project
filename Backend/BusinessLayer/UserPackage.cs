04202cfa048e71a6d6400f580c3e3cd0cdbec788
mamrud <mamrud@post.bgu.ac.il>
2020-04-13 18:38:54 +0300

    update stuff

--- Backend/BusinessLayer/UserPackage.cs
+++ Backend/BusinessLayer/UserPackage.cs
@@ -3,17 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IntroSE.Kanban.Backend.BusinessLayer.BoardPackage;
 
 namespace IntroSE.Kanban.Backend.BusinessLayer
 {
     namespace UserPackage
     {
+
+        class UserController
+        {
+            private Dictionary<string, User> UserList;
+            private User CurrentUser;
+
+            public UserController()
+            {
+
+            }
+
+            public User login(string Email, string Password)
+            {
+                if (UserList.ContainsKey(Email))
+                {
+                    User MyUser = UserList[Email];
+                    if (MyUser.ValidatePassword(Password))
+                    {
+                        CurrentUser = MyUser;
+                        return MyUser;
+                    }
+                    else
+                    {
+                        throw new Exception("your password is incorrect");
+                    }
+                }
+                else
+                {
+                    throw new Exception("there is no such user. please register.");
+                }
+
+            }
+
+            public void Logout(string Email)
+            {
+                if (!CurrentUser.GetEmail().Equals(Email))
+                {
+                    throw new Exception("only the active user can logout");
+                }
+                else
+                {
+                    CurrentUser = null;
+                }
+            }
+
+            public void Register(string Email, string Password, string NickName)
+            {
+                if (UserList.ContainsKey(Email))
+                {
+                    throw new Exception("this Email is already used");
+
+                }
+                else
+                {
+                    User MyUser = new User(Email, Password, NickName);
+                    UserList.Add(Email, MyUser);
+                }
+            }
+        }
+
         class User
         {
             private String email;
             private string password;
             private string nickname;
-            private myBoard Board;
+            private Board myBoard;
 
 
             public User(string email, string password, string nickname)
@@ -21,12 +82,12 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
                 this.email = email;
                 this.password = password;
                 this.nickname = nickname;
-                myBoard = new BoardPackage.Board();
+                myBoard = new Board();
             }
 
             public Boolean ValidatePassword(string password)
             {
-                return this.password == password;
+                return this.password.Equals(password);
             }
 
             public string GetEmail()
@@ -36,7 +97,7 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
 
             public Board GetBoard()
             {
-                return Board;
+                return myBoard;
             }
         }
     }
