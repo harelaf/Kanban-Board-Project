f5471b9ae65eb60e026f23e879e16f2e995d0da0
mamrud <mamrud@post.bgu.ac.il>
2020-04-17 17:14:34 +0300

    update

---
+++ Backend/DataAccessLayer/RegisteredEmails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Backend.DataAccessLayer
+{
+    class RegisteredEmail : DalObject<RegisteredEmail>
+    {
+        public List<string> Emails;
+        public DalController DCtrl;
+        public RegisteredEmail(DalController DCtrl,List<string> Emails)
+        {
+            this.DCtrl = DCtrl;
+            this.Emails = Emails;
+        }
+    }
+}
