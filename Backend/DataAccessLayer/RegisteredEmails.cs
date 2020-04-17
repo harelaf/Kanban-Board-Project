5b1db59eb9e14fb181a0e1c856a1a0b32b269741
Harel Afriat <harelaf@post.bgu.ac.il>
2020-04-17 17:57:07 +0300

    Changes the name of RegisteredEmail to RegisteredEmails

--- Backend/DataAccessLayer/RegisteredEmails.cs
+++ Backend/DataAccessLayer/RegisteredEmails.cs
@@ -3,17 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace IntroSE.Kanban.Backend.DataAccessLayer
 {
-    class RegisteredEmail : DalObject<RegisteredEmail>
+    class RegisteredEmails : DalObject<RegisteredEmails>
     {
         public List<string> Emails;
         public DalController DCtrl;
-        public RegisteredEmail(DalController DCtrl,List<string> Emails)
+        public const string FILE_NAME = "RegisteredEmails";
+        public RegisteredEmails(DalController DCtrl,List<string> Emails)
         {
             this.DCtrl = DCtrl;
             this.Emails = Emails;
+        }
+
+        public RegisteredEmails()
+        {
+            Emails = new List<string>();
+            DCtrl = new DalController();
+        }
+
+        public override string FromJson(string json)
+        {
+            return DCtrl.Read(json);
+        }
+
+        public override RegisteredEmails Import()
+        {
+            return JsonSerializer.Deserialize<RegisteredEmails>(FromJson(FILE_NAME));
+        }
+
+        public override void Save()
+        {
+            DCtrl.Write(FILE_NAME, ToJson());
         }
     }
 }
