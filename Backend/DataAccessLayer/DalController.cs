257832a9ffb6ec47f70c1cc3ce5c27d093f4e452
Harel Afriat <harelaf@post.bgu.ac.il>
2020-04-17 17:11:12 +0300

    Added DalController and part of User

---
+++ Backend/DataAccessLayer/DalController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IntroSE.Kanban.Backend.DataAccessLayer
+{
+    class DalController
+    {
+        public string Read(string filename)
+        {
+            return File.ReadAllText(filename);
+        }
+
+        public void Write(string filename, string content)
+        {
+            File.WriteAllText(filename, content);
+        }
+    }
+}
---
+++ Backend/DataAccessLayer/User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Backend.DataAccessLayer
+{
+    class User : DalObject<User>
+    {
+        public string email { get; set; }
+        private string password { get; set; }
+        public string nickname { get; set; }
+        public Board myBoard { get; set; }
+        public DalController dalController;
+
+        public User()
+        {
+            email = null;
+            password = null;
+            nickname = null;
+            myBoard = null;
+            dalController = new DalController();
+        }
+
+        public User(string email, string password, string nickname, Board myBoard)
+        {
+            this.email = email;
+            this.password = password;
+            this.nickname = nickname;
+            this.myBoard = myBoard;
+            dalController = new DalController();
+        }
+
+        public override void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override User Import()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string FromJson(string json)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
